0ec42142cfb0631010d9c21a0bdca552f10c2ecf
Leighton102 <76919670+Leighton102@users.noreply.github.com>
2021-01-15 09:02:57 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,8 +2,8 @@ module.exports = {
     name: 'help',
     description: "The help command!",
     execute(message, args){
-        message.channel.send(`I sent you a DM! ${message.author}`)
         message.author.send("**Join this server for help and commands!** https://discord.gg/9BdGSUPHja");
+        message.channel.send(`I sent you a DM! ${message.author}`)
 
         console.log(`${message.author.username} executed command help in ${message.guild.name}`)
 
