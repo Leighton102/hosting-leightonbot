1a05fe2ded081099d32073cee9f603fe2755bfbc
Leighton102 <76919670+Leighton102@users.noreply.github.com>
2021-01-16 14:29:28 -0500

    gsfdgdsfg
    dgsdgsdtgfg

--- index.js
+++ index.js
@@ -54,8 +54,6 @@ client.on('message', message => {
         client.commands.get('cheese').execute(message, args)
     } else if (command == 'mute') {
         client.commands.get('mute').execute(message, args)
-    } else if (command == 'rules') {
-        client.commands.get('rules').execute(message, args)
     }
 
 
